5511f85e818c323ce9ec4dc6e1da627c78345e7e
Rcass97 <rcass97@gmail.com>
2022-09-27 00:10:09 -0500

    headercomponent

---
+++ src/components/header/header.js
@@ -0,0 +1,22 @@
+import logo from '../../assets/LogoConcept.png';
+import headerlogo from '../../assets/headerbackground.jpg'
+import './header.css';
+
+function Header() {
+  return (
+    <div className="headerBackground">
+      <div className="headerBoxBull container">
+        <img src={logo} alt="up-on-jacks-logo" className="headerLogo" />
+        <ul className="headerList">
+          <li><a href>Sign Up</a></li>
+          <li><a href>Log In</a></li>
+        </ul>
+      </div>
+      <div className="hero container"><h2>Car website yes yes yes</h2><p>Once open, it could be closed for 30 to 45 minutes for car entries, but would not affect traffic into the mall or on the day.
+
+        Paul Joyce, the mall's executive director of marketing and business, told</p></div>
+    </div>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './components/header/header';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
