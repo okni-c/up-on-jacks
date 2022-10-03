9fc02b1694516e99bd70fab936692dc29e039739
okni-c <oknistay@gmail.com>
2022-10-02 21:53:12 -0500

    Footer Fixed

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,32 +2,35 @@ import logo from '../../assets/ModernLogoConcept.png';
 import twitter from '../../assets/tw-social-icon.png'
 import tiktok from '../../assets/tt-social-icon.png'
 import youtube from '../../assets/yt-social-icon.png'
+
+import { Link } from 'react-router-dom';
+
 import './footer.css';
 
 function Footer() {
     return (
-        <div className="footerbackground dflex">
-            <div className="container footercontainer dflex">
-                <div>
+        <div className='footerbackground'>
+            <div className='container footerContainer'>
+                <div id='footer1'>
                     <img src={logo} alt="up-on-jacks-logo" className="footerLogo" />
-                    <ul className="container footerlist dflex">
-                        <li>Legal</li>
-                        <li>About</li>
-                        <li>FAQ</li>
-                        <li>Careers</li>
+                    <ul className="footerlist">
+                        <li><Link to="/">Legal</Link></li>
+                        <li><Link to="/">About</Link></li>
+                        <li><Link to="/">FAQ</Link></li>
+                        <li><Link to="/">Careers</Link></li>
                     </ul>
                 </div>
-                <div>
-                    <h2 className="">Follow Us!</h2>
-                    <ul className="footersocial dflex">
-                        <li><img src={twitter} alt="" /></li>
-                        <li><img src={tiktok} alt="" /></li>
-                        <li><img src={youtube} alt="" /></li>
+                <div id='footer2'>
+                    <h2>Follow Us!</h2>
+                    <ul className="footersocial">
+                        <li><img src={twitter} alt="Twitter" id='tw' /><Link to="" className='link-spanner' aria-labelledby='tw' title='Twitter' /></li>
+                        <li><img src={tiktok} alt="TikTok" id='tt' /><Link to="" className='link-spanner' aria-labelledby='tt' title='TikTok' /></li>
+                        <li><img src={youtube} alt="YouTube" id='yt' /><Link to="" className='link-spanner' aria-labelledby='yt' title='YouTube' /></li>
                     </ul>
                 </div>
             </div>
-
         </div>
+
 
     );
 }
